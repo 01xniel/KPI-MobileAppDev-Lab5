5155b39aa6c5cacc07e5868acad4534da3197f9a
01xniel <dnl.dzbn@gmail.com>
2024-11-26 04:57:06 +0200

    unused parameter was removed

--- app/src/main/java/com/example/kpi_mobileappdev_lab5/styledcomponents/ButtonText.kt
+++ app/src/main/java/com/example/kpi_mobileappdev_lab5/styledcomponents/ButtonText.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -15,7 +14,6 @@ fun ButtonText(text: String) {
         text,
         fontSize = 16.sp,
         lineHeight = 25.sp,
-        textAlign = TextAlign.Justify,
         modifier = Modifier.padding(vertical = 15.5.dp)
     )
 }
